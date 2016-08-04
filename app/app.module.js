65daec937c69b22cbc70d1b55a5c71733c0698cd
Matti Kankkonen <matti.kankkonen@gmail.com>
2016-08-04 16:33:54 +0300

    Added image-list component initial version

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,6 @@
-angular.module('tangularFlow', [])
+angular.module('tangularFlow', [
+'imageList'
+	])
 .controller('TangularController', function 
 	TangularController($scope) {
 	$scope.tensorServer="http://tensorshow.herokuapp.com";
---
+++ app/image-list/image-list.component.js
@@ -0,0 +1,4 @@
+angular.module('imageList')
+.component('imageList', {
+	templateUrl:'image-list/image-list.template.html'
+});
