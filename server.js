b292c876fe75a2a7c4fca1c6777cdad8127e05f9
Matti Kankkonen <matti.kankkonen@gmail.com>
2016-08-04 13:23:06 +0300

    Initial setup, node serving static page from app directory

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static('app'));
+app.get('/hello', function (req,resp) {
+	resp.send('Hello there\n');
+});
+
+app.listen(PORT);
