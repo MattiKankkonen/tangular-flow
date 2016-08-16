84827a4da0fc7cf96da06ea49019af9dc9455bee
Matti Kankkonen <matti.kankkonen@gmail.com>
2016-08-16 14:02:36 +0300

    Added Tensorflow service use to image poster

--- app/image-post/image-post.component.js
+++ app/image-post/image-post.component.js
@@ -3,9 +3,10 @@
 angular.module('imagePost')
 .component('imagePost', {
   templateUrl: 'image-post/image-post.template.html',
-  controller: ['$http', function ImagePostController($http) {
+  controller: ['$http', 'Tensorflow', function ImagePostController($http, Tensorflow) {
     var self = this;
     self.imageCaption = "If you upload a picture it comes here";
+    self.analysis = '';
     self.http = $http;
 
     self.postImage = function(uploadFile) {
@@ -19,6 +20,22 @@ angular.module('imagePost')
       .success(function() {
         self.imageCaption = 'Here is the uploaded image';
         self.imageUrl = '/images/img.jpg?random=' + Date.now();
+        self.analysis = 'Uploading your image to TensorFlow engine... please wait for results';
+        // TODO: should we prevent caching also here with ?random= !!!
+        // TODO: the image URL host part obviously needs to be something else i.e. resolve the host name
+        self.data = Tensorflow.getResource().
+          get({imgurl: 'http://localhost:3000/images/img.jpg'}, function(data) {
+          if(data.status === "valid") {
+            self.analysis = "TensorFlow says this is " + 
+            data.result + " with probability of " +
+            data.probability;
+            } else {
+              self.analysis = "Failed to analyse picture";
+            }
+          }, function () {
+            self.analysis = "Networking error with TensorFlow engine";
+          }
+        );
       })
       .error(function() {
         self.imageCaption = 'Upload failed';
--- app/image-post/image-post.module.js
+++ app/image-post/image-post.module.js
@@ -1,2 +1,2 @@
 'use strict';
-angular.module('imagePost', []);
+angular.module('imagePost', ['core.tensorflow']);
