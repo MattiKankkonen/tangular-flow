efd8bf71d110ca4c8d2fc62eb1fb1f0bed66f6aa
Matti Kankkonen <matti.kankkonen@gmail.com>
2016-08-16 12:06:37 +0300

    Image posting functionality implemented

--- app/image-post/image-post.component.js
+++ app/image-post/image-post.component.js
@@ -2,5 +2,42 @@
 
 angular.module('imagePost')
 .component('imagePost', {
-  templateUrl: 'image-post/image-post.template.html'
-});
+  templateUrl: 'image-post/image-post.template.html',
+  controller: ['$http', function ImagePostController($http) {
+    var self = this;
+    self.imageCaption = "If you upload a picture it comes here";
+    self.http = $http;
+
+    self.postImage = function(uploadFile) {
+      self.imageCaption ="Now posting picture " + uploadFile.name;
+      var fd = new FormData();
+      fd.append('file', uploadFile);
+      self.http.post('/imgsrv', fd, {
+        transformRequest: angular.identity,
+        headers: {'Content-Type:': undefined }
+      })
+      .success(function() {
+        self.imageCaption = 'Here is the uploaded image';
+        self.imageUrl = '/img.jpg?random=' + Date.now();
+      })
+      .error(function() {
+        self.imageCaption = 'Upload failed';
+      });
+    }
+  }]
+})
+.directive('fileModel', ['$parse', function($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      var modelSetter = model.assign;
+      
+      element.bind('change', function() {
+        scope.$apply(function() {
+          modelSetter(scope, element[0].files[0]);
+        });
+      });
+    }
+  };
+}]);
